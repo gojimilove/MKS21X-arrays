1f3ceebf3bc085b2bf83c7ad4c80ad1c13c50dd5
Jessica Kim <jkim00@stuy.edu>
2018-09-26 21:17:56 -0400

    working printArray(int[]ary) function

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,1 +1,17 @@
+public class ArrayDemo {
 
+	public static void printArray(int[]ary) {
+		System.out.print("[");
+		for (int i = 0; i < ary.length; i++) {
+			System.out.print(ary[i] + ", ");
+		}
+		System.out.print("]\n");
+
+	}
+
+	public static void main(String[] args) {
+		printArray(new int[]{1, 2, 3, 4, 5});
+
+	}
+
+}
