2ad183acda9cf4343d8247edf03366e6648014d0
Jessica Kim <jkim00@stuy.edu>
2018-09-26 22:49:12 -0400

    working fill2D(int[][]vals) function

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -28,14 +28,41 @@ public class ArrayDemo {
 			}
 		}
 		return result;
+	
+	}
+
+	public static void fill2D(int[][]vals) {
+		for (int i = 0; i < vals.length; i++) {
+			for (int j = 0; j < vals[i].length; j++) {
+				if (i == j) {
+					vals[i][j] = 3;
+				}
+				else {
+					vals[i][j] = 1;
+				}
+			}
+		}
+		printArray(vals);
+
 	}
 
 	public static void main(String[] args) {
+		printArray(new int[]{1, 2, 3, 4, 5});
+		printArray(new int[][]{
+													{1, 2, 3, 4, 5},
+													{6, 7, 8, 9, 10},
+													{11, 12, 13, 14, 15},
+													});
 		System.out.println(countZeros2D(new int[][]{
 													{1, 0, 3, 0, 5},
 													{6, 7, 0, 0, 0},
 													{11, 0, 13, 14, 15},
 													}));
+		fill2D(new int[][] {
+													{1, 2, 3, 4, 5},
+													{6, 7, 8, 9, 10},
+													{11, 12, 13, 14, 15},
+													});
 
 	}
 
